9d5a229a7b6fb12dde39d902621954635fc39cb4
Rama <ramanathanpri@gmail.com>
2021-05-29 11:23:04 +0530

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import Header from './components/Header';
+import { DISHES } from './shared/dishes';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div >
-        <Header/> 
-        <Menu/>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div >
+          <Header/> 
+          <Menu dishes={this.state.dishes} />
+  
+      </div>
+    );
+  }
+  
+  
 }
+
 
 export default App;
